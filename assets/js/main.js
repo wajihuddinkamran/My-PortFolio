b53dd9efa4d31072e4305dfc7b3fafb3ab53202d
M.W.Kamran <74808095+wajihuddinkamran@users.noreply.github.com>
2024-12-26 10:17:45 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@
 
 /* ----- TYPING EFFECT ----- */
    var typingEffect = new Typed(".typedText",{
-      strings : ["Designer","Youtuber","Developer"],
+      strings : ["Designer","Youtuber","Developer","Ecom"],
       loop : true,
       typeSpeed : 100, 
       backSpeed : 80,
